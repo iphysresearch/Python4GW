1cf6b147a8e4fac8bde649d5950df6c94a55f241
MAC_Herb <hewang@mail.bnu.edu.cn>
2018-10-25 20:55:12 +0800

    random+noise+matlab

--- data_noise.py
+++ data_noise.py
@@ -137,8 +137,9 @@ def fftfilt(b, x, nfft=None):
     if nfft == None:
         # figure out which nfft and L to use
         if (nb >= nx) or (nb > 2**20):  # take a single FFT in this case
-            nfft = 2**round(np.log(nb+nx-1)/np.log(2))
+            nfft = int(2**round(np.log(nb+nx-1)/np.log(2)))
             L = nx
+            print(11111)
         else:
             fftflops = np.array([ 18,59,138,303,660,1441,3150,6875,14952,32373,69762,
                                  149647,319644,680105,1441974,3047619,6422736,13500637,
@@ -151,7 +152,7 @@ def fftfilt(b, x, nfft=None):
             L = n - (nb - 1)
             temp = np.ceil(nx/L) * fftflops
             dum, ind = np.min(temp), np.argmin(temp)
-            nfft = n[ind]
+            nfft = int(n[ind])
             L = L[ind]
             
     else:  # nfft is given
@@ -297,3 +298,149 @@ def fir2(nn, ff, aa, **kwargs):
     b = b * wind.T  # Apply window. 
     a = 1           # Denominator. 
     return b
+
+def pre_fir(targetSens = 'ZERO_DET_high_P.txt', fLow=20, fHigh=9000, fs = 8192, filtOrdr = 100):
+    """
+    Obtain filter coefficients for preparing to generate the colored noises. (ALL NUMPY)
+    Input:
+    - targetSens: str. File containing target sensitivity curve (first column is frequency and
+                  second column is square root of PSD).
+    - fLow: Select low pass. Default as 20 Hz.
+    - fHigh: Select high pass. Default as 9000 Hz.
+    - fs: sample of rate. Default as 8192 Hz
+    - filterOrdr: FIR filter order
+    
+    Output:
+    - b: filter coefficients. Default as an np.array in shape of (101,)
+    """
+    targetSens = np.loadtxt(targetSens)
+    targetSens[(targetSens[:,0] <= fLow) + (targetSens[:,0] >=fHigh), 1] = 0
+
+    indxfCut = (targetSens[:,0] <= fs/2)
+    targetSens = targetSens[indxfCut, :]
+
+    if targetSens[0,0] > 0:
+        addZero = 1
+    else:
+        addZero = 0
+    if targetSens[-1,0] < fs/2:
+        addNyq = 1
+    else:
+        addNyq = 0
+
+    if addZero:
+        targetSens = np.concatenate((np.zeros((1,2)), targetSens))
+    if addNyq:
+        targetSens = np.concatenate((targetSens, np.array([[fs/2, 0]])))
+
+    b = fir2(filtOrdr, targetSens[:,0]/(fs/2), targetSens[:,1])
+    return b
+
+
+def GenNoise_matlab_np(shape, b):
+    """
+    Generate noise(ONLY NUMPY).
+    Pass a white noise sequence through the designed filter 'b'.
+    Input:
+    - shape: a tuple indicate the shape of sample noises.
+    - b: filter coefficients. (see pre_fir() for details)
+    Output:
+    - outputNoise: ndarray with the shape of 'shape'.
+    """    
+    (numsamples, numsamplepoints) = shape
+    inputNoise = np.random.randn(numsamples, numsamplepoints)
+    outputNoise = fftfilt(b.reshape((-1,1)),inputNoise.T).T
+    return outputNoise
+
+
+def GenNoise_matlab_nd(shape, b):
+    """
+    Generate noise(ONLY MXNet GPU).
+    Pass a white noise sequence through the designed filter 'b'.
+    Input:
+    - shape: a tuple indicate the shape of sample noises.
+    - b: filter coefficients (in mx.ndarray). (see pre_fir() for details)
+    Output:
+    - outputNoise: mx.ndarray with the shape of 'shape'.
+    """
+    (numsamples, numsamplepoints) = shape
+    inputNoise = np.random.randn(numsamples, numsamplepoints)
+    outputNoise = fftfilt_nd(b.reshape((-1,1)),inputNoise.T).T
+    return outputNoise
+
+def fftfilt_nd(b, x, nfft=None):
+    m = x.shape[0]
+    if m == 1:
+        x = x.reshape((-1,1))  # turn row into a column
+    nx = x.shape[0]
+    
+    if min(b.shape) > 1:
+        assert b.shape[1] == x.shape[1] and x.shape[1] <=1, "signal:fftfilt:InvalidDimensions"
+    else:
+        b = b.reshape((-1,1))   # make input a column
+    nb = b.shape[0]
+    
+    if nfft == None:
+        # figure out which nfft and L to use
+        if (nb >= nx) or (nb > 2**20):  # take a single FFT in this case
+            nfft = int(2**round(np.log(nb+nx-1)/np.log(2)))
+            L = nx
+        else:
+            fftflops = nd.array([ 18,59,138,303,660,1441,3150,6875,14952,32373,69762,
+                                149647,319644,680105,1441974,3047619,6422736,13500637,
+                                28311786,59244791,59244791*2.09])
+            n = 2**nd.arange(1,22,1)
+            validset_first = nd.argmax(n>nb-1,axis=0).asscalar()
+            n = nd.slice(n, begin=[int(validset_first),], end=(None,))
+            fftflops = nd.slice(fftflops, begin=[int(validset_first),], end=(None,))
+            # minimize (number of blocks) * (number of flops per fft)
+            L = n - (nb - 1)
+            temp = nd.ceil(nx/L) * fftflops
+            dum, ind = nd.min(temp), nd.argmin(temp, axis=0)
+            nfft = int(n[int(ind.asscalar())].asscalar())
+            L = int(L[int(ind.asscalar())].asscalar())
+            
+    else:  # nfft is given
+        # Cast to enforce precision rules
+        pass
+        raise 'nfft is given?'
+        '''
+        nfft = signal.internal.sigcasttofloat(nfft,'double','fftfilt','N','allownumeric');
+        if nfft < nb
+            nfft = nb;
+        end
+        nfft = 2.^(ceil(log(nfft)/log(2))); % force this to a power of 2 for speed
+        L = nfft - nb + 1;        
+        '''
+    # Check the input data type. Single precision is not supported.
+    '''
+    try
+        chkinputdatatype(b,x,nfft);
+    catch ME
+        throwAsCaller(ME);
+    end'''
+    B = fft(b.T, nfft).T
+    if b.size == 1:
+        B = B.T     # make sure fft of B is a column (might be a row if b is scalar)
+    if b.shape[1] == 1:
+        B = np.repeat(B, [x.shape[1],],axis=1)    # replicate the column B 
+    if x.shape[1] == 1:
+        x = np.repeat(x, [b.shape[1],],axis=1)   # replicate the column x 
+    y = np.zeros_like(x)
+
+    istart = 1
+    while istart <= nx:
+        iend = min(istart+L-1, nx)
+        if (iend - istart) == 0:
+            X = x[istart] * np.ones((nfft,1))  # need to fft a scalar
+        else:
+            X = fft(x[istart-1:iend,:].T, nfft).T
+        Y = ifft((X * B).T).T
+        yend = min(nx,istart+nfft-1)
+        y[istart-1:yend,:] = y[istart-1:yend,:] + Y[:(yend-istart+1),:]
+        istart += L
+    y = real(y)
+    if (m == 1) and (y.shape[1] == 1):
+        y = y.T    # turn column back into a row
+    return y
+    
